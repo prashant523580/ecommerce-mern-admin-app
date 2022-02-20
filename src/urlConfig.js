bf62e759f57d3c40ec3a5c62b9268ee3f1c2b57b
root <root-_-@2400-1A00-B1C0-ip6.wlink.com.np>
2022-02-20 23:48:21 +0545

    add backend api

--- src/urlConfig.js
+++ src/urlConfig.js
@@ -1,4 +1,4 @@
-export const api = "http://192.168.100.71:4444/api";
+export const api = "https://ecommercemernbackend.herokuapp.com/api";
 export const generateImgUrl = (image) => {
-    return `http://192.168.100.71:4444/public/${image}`;
+    return `https://ecommercemernbackend.herokuapp.com/public/${image}`;
 }
